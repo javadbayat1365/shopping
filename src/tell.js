e875053d99f20c222ff1193353c810a30ce7ae15
javad <javadbayat.bj@outlook.com>
2019-01-03 08:30:31 -0800

    LifeSycle Components
    componentWillAmount
    componentDidAmount
    componentWillUpdate
    componentDidUpdate
    ShouldcomponentUpdate
    ....

--- src/tell.js
+++ src/tell.js
@@ -6,7 +6,7 @@ import Mixins from './Mixins';
 
 const FormItem = Form.Item;
 
- class AboutUs extends Component {
+class AboutUs extends Component {
 
     constructor(props) {
         super(props);
@@ -16,7 +16,11 @@ const FormItem = Form.Item;
                 name: 'javad',
                 email: '',
                 message: ''
-            }
+            },
+            counter: 0
+
+
+
         }
 
         this.handleSubmit = this.handleSubmit.bind(this);
@@ -25,23 +29,27 @@ const FormItem = Form.Item;
         this.success("hello Mixins");
         this.error("I'm ready for using Mixins");
     }
-    
-    handlechange(event){
-      const target = event.target;
-      const name = target.name;
-      const value = target.value;
 
-    this.state.formBody[name] = value;
-    
-    this.setState({
-      formBody : this.state.formBody
-    })
+    handlechange(event) {
+        const target = event.target;
+        const name = target.name;
+        const value = target.value;
+
+        this.state.formBody[name] = value;
+
+        this.setState({
+            formBody: this.state.formBody
+        })
     }
-   
+
     handleSubmit(e) {
         e.preventDefault();
         console.log(this.state.formBody);
 
+
+        this.setState({
+            counter: this.state.counter + 1
+        })
     }
 
 
@@ -56,7 +64,7 @@ const FormItem = Form.Item;
                             <h2>Contact US</h2>
                             <Form onSubmit={this.handleSubmit}>
                                 <FormItem label="name">
-                                    <Input type="text" name="name" value={this.state.formBody.name} onChange={this.handlechange}  placeholder="please Enter Your Name" />
+                                    <Input type="text" name="name" value={this.state.formBody.name} onChange={this.handlechange} placeholder="please Enter Your Name" />
                                 </FormItem>
                                 <FormItem label="e-mail">
                                     <Input type="text" value={this.state.formBody.email} onChange={this.handlechange} name="email" placeholder="please Enter Your e-mail" />
@@ -67,6 +75,7 @@ const FormItem = Form.Item;
                                 <FormItem >
                                     <Button htmlType="submit" type="primary" >Click ME!</Button>
                                 </FormItem>
+                                <Content mynumber={this.state.counter} />
                             </Form>
                         </Col>
                     </Row>
@@ -77,6 +86,54 @@ const FormItem = Form.Item;
     }
 }
 
+class Content extends React.Component {
+    componentWillMount() {
+        this.success("Component Will Mount");
+    }
+    componentDidMount() {
+        this.error("Component Did Mount");
+    }
+     
+    componentWillUpdate(){
+        this.success("Component WIll Update");
+    }
 
-ReactMixin.onClass(AboutUs,Mixins.log);
+    componentDidUpdate(){
+        this.error("Component DId Update");
+    }
+
+
+    componentWillReceiveProps(){
+        if(this.shouldComponentUpdate() === true)
+     this.success("Component WIll Receive Props");
+     else
+     this.error("Component WIll not Receive Props");
+    }
+
+    componentWillUnmount()
+    {
+        this.error("Component WIll Unmount");
+    }
+
+    shouldComponentUpdate()
+    {
+        return true;
+    }
+
+
+    constructor(props) {
+        super(props);
+    }
+    render() {
+        return (
+            <FormItem label="Counter Click">
+                <label type="lable">{this.props.mynumber}</label>
+            </FormItem>
+        )
+    }
+}
+
+
+ReactMixin.onClass(Content,Mixins.log);
+ReactMixin.onClass(AboutUs, Mixins.log);
 export default AboutUs;
